ae8f3914c9f316807790beb4f404960711e66a65
Sanjay Atholi <sanjaysr2902@gmail.com>
2026-04-15 04:41:11 +0530

    cors fixed finally

--- src/main/java/application/demo/CorsConfig.java
+++ src/main/java/application/demo/CorsConfig.java
@@ -1,3 +1,5 @@
+package application.demo;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.*;
@@ -8,6 +10,7 @@ public class CorsConfig {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
+
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
