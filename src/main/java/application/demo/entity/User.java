7c34c765d311dc64976517b51b62b4998c04e084
Sanjay Atholi <sanjaysr2902@gmail.com>
2026-04-26 01:21:25 +0530

    vv

--- src/main/java/application/demo/entity/User.java
+++ src/main/java/application/demo/entity/User.java
@@ -11,6 +11,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private String role; // "USER" or "ADMIN"
     @Column(unique = true)
     private String email;
     private String password;
